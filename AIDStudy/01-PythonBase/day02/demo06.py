d8b8be0e4102f6b196557361ad586d5169909751
fanxiao168 <fanxiao168@126.com>
2020-02-28 16:26:45 +0800

    change folder

---
+++ AIDStudy/01-PythonBase/day02/demo06.py
@@ -0,0 +1,20 @@
+'''
+增强运算
+'''
+
+num = 200
+print(num + 1)  # 201
+print(num)  # 200
+
+# 增强运算
+# 累计
+# 变量的值加上另一个数，再赋值给自身变量
+# num = num + 1
+num += 1
+print(num)
+
+num -= 1
+print(num)
+
+num *= 2
+print(num)
